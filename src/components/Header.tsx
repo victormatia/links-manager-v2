546f7d744de2c88aaef51b042ceaa378bd1c71cf
victormatia <fvictor1705@gmail.com>
2024-06-12 15:27:49 -0300

    styles: decreases padding bottom of the Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import BioComponent from "./BioComponent";
 
 export default function Header() {
   return (
-    <header className='flex flex-col items-center justify-center w-full bg-gradient-gray-to-transparent p-6 gap-4'>
+    <header className='flex flex-col items-center justify-center w-full bg-gradient-gray-to-transparent p-6 pb-4 gap-2'>
       <Image className='w-24 h-24 object-cover rounded-full' src={ profile } alt="profile" />
       <span className='text-2xl font-semibold'>Vitória Maia</span>
       <BioComponent />
